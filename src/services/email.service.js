f86cf6008600934ad007efe70dfeb999d94f1219
Mudassir <mudassir.code.in@gmail.com>
2026-06-14 14:47:22 +0530

    add brevo service

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -3,9 +3,9 @@ import nodemailer from 'nodemailer';
 
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,             
-    secure: true,
+    host: 'smtp-relay.brevo.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
