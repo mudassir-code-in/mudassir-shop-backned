a48e8ea69071133be2745153cc37c97510b3e177
Mudassir <mudassir.code.in@gmail.com>
2026-06-14 12:34:46 +0530

    fix transport verify

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ dotenv.config();
 import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
 import { connectRedis } from './src/config/redis.js';
-import { transporter } from './src/services/email.service.js';
+
 
 
 
@@ -17,11 +17,5 @@ const port = 3000
 
 app.listen(port, () => {
     console.log('server is running on 3000 port');
-    transporter.verify((error, success) => {
-        if (error) {
-            console.log('Email server connecting error', error);
-        } else {
-            console.log('Email server ready to send message');
-        }
     });
-});
+
--- src/services/email.service.js
+++ src/services/email.service.js
@@ -2,8 +2,8 @@ import nodemailer from 'nodemailer';
 
 
 
-export const transporter = nodemailer.createTransport({
-    host: 'smtp-relay.brevo.com', 
+const transporter = nodemailer.createTransport({
+    host: 'smtp-relay.brevo.com',
     port: 587,
     auth: {
         user: process.env.EMAIL_USER,
@@ -11,20 +11,27 @@ export const transporter = nodemailer.createTransport({
     }
 });
 
-
-export const sendMail = async (to, subject, text, html) => {
-    try{
-        const info = await transporter.sendMail({
-            from: `Mudassir developer, ${process.env.EMAIL_USER}`,
-            to,
-            subject,
-            text,
-            html
-        });
-
-    } catch (error) {
-        console.log('Email sending error', error);
-
-        throw error;
+transporter.verify((error, success) => {
+    if (error) {
+        console.log('Email server connecting error', error);
+    } else {
+        console.log('Email server ready to send message');
     }
-}
+});
+
+    export const sendMail = async (to, subject, text, html) => {
+        try {
+            const info = await transporter.sendMail({
+                from: `Mudassir developer, ${process.env.EMAIL_USER}`,
+                to,
+                subject,
+                text,
+                html
+            });
+
+        } catch (error) {
+            console.log('Email sending error', error);
+
+            throw error;
+        }
+    }
