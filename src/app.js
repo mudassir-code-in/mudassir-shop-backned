ec6900278b0fb08a9cc5b59530316135147b5b95
Mudassir <mudassir.code.in@gmail.com>
2026-06-14 10:22:12 +0530

    origin cors

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'https://vintage-pi.vercel.app/', 
+    origin: 'http://mudassir-shop.vercel.app/', 
     credentials: true,             
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
